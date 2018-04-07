b7aca41a948126eff5dc854e9354ac7228d05b91
verchol <verchol@gmail.com>
2018-04-07 22:43:22 +0300

    fix

--- scripts/newrelease.sh
+++ scripts/newrelease.sh
@@ -1,3 +1,13 @@
 commit=$1
 #echo 'looking for match for' $1
-git describe --exact-match $1
+ERROR="fatal: no tag exactly matches"
+git describe --exact-match $commit 2>v 1>v1
+TAG=$(cat v)
+echo $(cat v1)
+echo tag $TAG
+if echo "$TAG" | grep -q "$ERROR"; then
+  echo "no tag detected";
+else
+  echo "there is tag";
+  echo TAG=$(cat v1)>/codefresh/volume/cache/tag
+fi
--- v
+++
@@ -1,6 +0,0 @@
-set cf default release version  
-volume = /codefresh/volume/cache
-1.0.8
-----
-current version is  1.0.8
-1.0.8
